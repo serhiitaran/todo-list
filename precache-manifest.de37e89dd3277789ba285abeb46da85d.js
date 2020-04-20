bee83b8708cfe32a0d600a629b9131b62bf59ffb
Serhii Taran <serjtaran@gmail.com>
2020-04-20 20:27:56 +0300

    Updates

---
+++ precache-manifest.de37e89dd3277789ba285abeb46da85d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcb70723e4362a6ac27efdf28c98776",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "4600b14ec6a87a95bcea",
+    "url": "/todo-list/static/js/2.5d0a5a6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.5d0a5a6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76416123d18c02aa1734",
+    "url": "/todo-list/static/js/main.49a55427.chunk.js"
+  },
+  {
+    "revision": "9c00833b61171e0ba36d",
+    "url": "/todo-list/static/js/runtime-main.c17e1442.js"
+  }
+]);
