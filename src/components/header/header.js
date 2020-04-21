acf68e09c9d1a5a4184f868c804d4ab47634f9f6
Serhii Taran <serjtaran@gmail.com>
2020-04-21 21:50:42 +0300

    fix(Header component): jsx class names

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,9 +3,9 @@ import React from 'react';
 export const Header = props => {
   const { done, active } = props;
   return (
-    <header class="header">
-      <h1 class="header__title">To do List</h1>
-      <p class="header__info">
+    <header className="header">
+      <h1 className="header__title">To do List</h1>
+      <p className="header__info">
         <span>Done: {done}</span> <span>Active: {active}</span>
       </p>
     </header>
