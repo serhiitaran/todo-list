2cfcd0035985268ecfd6fb2f3ae0f123c3fa4ac9
Serhii Taran <serjtaran@gmail.com>
2020-04-22 19:59:43 +0300

    feat(App component): create state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,13 @@ import Header from '../header';
 import './app.css';
 
 export class App extends Component {
+  state = {
+    items: [
+      { id: 1, label: 'First to do item', done: true },
+      { id: 2, label: 'Second to do item', done: true },
+      { id: 3, label: 'Third to do item', done: false },
+    ],
+  };
   render() {
     return (
       <div className="app">
